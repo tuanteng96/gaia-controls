c984d4fa170fdb36a7fbf5e5d3c71b921b7716f2
tuanteng96 <tuantengyn@gmail.com>
2022-03-16 10:44:38 +0700

    update

--- src/features/Auth/_redux/AuthInit.js
+++ src/features/Auth/_redux/AuthInit.js
@@ -1,15 +1,26 @@
 import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 import { LayoutSplashScreen } from "../../../layout/_core/EzsSplashScreen";
+import { setToken } from "./AuthSlice";
+
+const tokenFake =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJUb2tlbklEIjoiNjAxIiwibmJmIjoxNjQ3NDAxNDIyLCJleHAiOjE2Nzg5Mzc0MjIsImlhdCI6MTY0NzQwMTQyMn0.PeMBLwTAfpl-xuwFda3ziwJhHbtyEd2IFRD25_-xP3g";
 
 function AuthInit(props) {
+  const dispatch = useDispatch();
   const [showSplashScreen, setShowSplashScreen] = useState(true);
 
   // We should request user by authToken before rendering the application
 
   useEffect(() => {
-    if (Number("1") !== 1) {
+    const requestUser = () => {
+      dispatch(setToken(window.Token || tokenFake));
+      setShowSplashScreen(false);
+    };
+
+    if (window.Token || tokenFake) {
       // Xử lí
-      setShowSplashScreen(true);
+      requestUser();
     } else {
       setShowSplashScreen(false);
     }
--- src/features/Auth/_redux/AuthSlice.js
+++ src/features/Auth/_redux/AuthSlice.js
@@ -2,17 +2,17 @@ import { createSlice } from "@reduxjs/toolkit";
 import persistReducer from "redux-persist/es/persistReducer";
 import storage from "redux-persist/lib/storage";
 
-const tokenFake = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJUb2tlbklEIjoiNTg4IiwibmJmIjoxNjQ3MjI0ODk0LCJleHAiOjE2Nzg3NjA4OTQsImlhdCI6MTY0NzIyNDg5NH0.E70WFM_UAq9-TOPVkTaZCPyLXqP1xdbGYBpGK-mbrDQ';
-
 const auth = createSlice({
     name: "auth",
     initialState: {
-        Token: window.Token || tokenFake
+        Token: window.Token
     },
     reducers: {
-        logoutUser: (state, action) => {
-            localStorage.removeItem("_info_review");
-            window.location.href = process.env.REACT_APP_API_URL + "/on-tap.aspx"
+        setToken: (state, action) => {
+            return {
+                ...state,
+                Token: action.payload
+            }
         }
     }
 });
@@ -24,5 +24,5 @@ const persistConfig = {
 };
 
 const { reducer, actions } = auth;
-export const { setUser, logoutUser } = actions;
+export const { setToken } = actions;
 export default persistReducer(persistConfig, reducer);
